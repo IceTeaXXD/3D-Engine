6c310c744acca469d241a8d0a2ebf0f396e1258a
Ahmad Nadil <13521024@std.stei.itb.ac.id>
2024-05-25 20:41:12 +0700

    fix: models

--- src/script/app/models/index.js
+++
@@ -1,7 +0,0 @@
-import { ArticulatedModel } from "../../modules/objects/ArticulatedModel/index.js"
-import fox from "./fox.js"
-
-const model = ArticulatedModel.fromModelDefinition(fox)
-model.scale.mul(5)
-
-export default model
